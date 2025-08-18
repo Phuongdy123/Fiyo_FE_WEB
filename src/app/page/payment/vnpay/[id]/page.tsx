f1b98ec1582f29276bb8b8b0670b8f1162614da3
Nguyễn Duy Phương <phuongndps39487@gmail.com>
2025-08-18 19:43:16 +0700

    Update page.tsx
    ik

--- src/app/page/payment/vnpay/[id]/page.tsx
+++ src/app/page/payment/vnpay/[id]/page.tsx
@@ -61,7 +61,7 @@ export default function VnpayPage() {
 
     try {
       const res = await fetch('http://localhost:3000/api/orders/vnpay', {
-        method: 'POST',
+          method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(orderData),
       });
